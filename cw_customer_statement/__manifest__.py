21745e3e2d930d519a0f39c8184072c00a42ab38
Gonzalo <109755692+gonzalosiu@users.noreply.github.com>
2024-04-04 16:53:16 +0800

    add customer statement

---
+++ cw_customer_statement/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "CW - Customer Statement",
+    "author": "Goja Solutions",
+    "category": "Accounting/Accounting",
+    "license": "AGPL-3",
+    "summary": "Report Customer Statement",
+    "description": """Report Customer Statement.""",
+    "version": "16.0.1.0.0",
+    'depends': ['account_reports', 'sale'],
+    "data": [
+        'views/res_partner_views.xml',
+        'reports/paperformat.xml',
+        'reports/report_customer_statement.xml',
+        'reports/report_multi_customer_statement.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ cw_customer_statement/models/res_partner.py
@@ -0,0 +1,55 @@
+from odoo import models, fields, api, _
+from datetime import datetime
+
+class ResPartnerCategory(models.Model):
+    _inherit = "res.partner.category"
+
+    gjs_company_name = fields.Char(string="Company Name")
+    gjs_address = fields.Text(string="Address")
+    gjs_bank_information = fields.Text(string="Bank Information")
+    gjs_bank_qr_code = fields.Image(string="Bank QR Code")
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    @api.model
+    def _get_address_format(self):
+        if self.env.context.get('customer_statement'):
+            return """
+                %(street)s
+                %(street2)s %(city)s %(state_code)s %(zip)s %(country_name)s
+            """
+        return super(ResPartner, self)._get_address_format()
+
+    def _get_statement_company(self):
+        return self and self[0].company_id or self.env.company
+
+    def _get_report_customer_statement_filename(self):
+        self.ensure_one()
+        today = datetime.strftime(datetime.now(), '%d%m%Y')
+        return '%s-%s' % (self.name, today)
+
+    def _get_aged_receivable_data(self):
+        self.ensure_one()
+        data = []
+        ar = self.env.ref('account_reports.aged_receivable_report')
+        options = ar._get_options()
+        options['unfold_all'] = True
+        options['partner'] = True
+        options['partner_ids'] = [self.id]
+        options['selected_partner_ids'] = [self.commercial_partner_id.name]
+        ar_model = self.env['account.aged.receivable.report.handler']
+        rslt = ar_model._aged_partner_report_custom_engine_common(
+            options, 'asset_receivable', 'partner_id', 'id')
+        for r in rslt:
+            if len(r) <= 1 or not isinstance(r[1], dict):
+                continue
+            data.append(r[1])
+        return data
+
+    def _get_studio_po_number_field(self):
+        so_fields = self.env['sale.order']._fields.keys()
+        for sof in so_fields:
+            if 'po_number' in sof:
+                return sof
+        return False
