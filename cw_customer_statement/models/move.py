5c48884f62c82cf7ae666ea26e1ec2175c66b752
Gonzalo <109755692+gonzalosiu@users.noreply.github.com>
2024-04-04 16:53:16 +0800

    add customer statement

---
+++ cw_customer_statement/models/move.py
@@ -0,0 +1,16 @@
+from odoo import models, fields, api, _
+from datetime import datetime
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _get_inv_po_number(self, field_name=''):
+        self.ensure_one()
+        if not field_name:
+            return self.ref
+        source_orders = self.line_ids.sale_line_ids.order_id
+        for order_dict in source_orders.read():
+            if order_dict.get(field_name):
+                return order_dict.get(field_name)
+        return self.ref
