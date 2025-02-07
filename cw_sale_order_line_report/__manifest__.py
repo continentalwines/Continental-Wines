cc9a80a98b5153e503f2673f521762f9c332d1cc
Gonzalo Siu <gonzalo.siu@gmail.com>
2025-02-07 16:21:57 +0700

    [add] sales order line pivot view

---
+++ cw_sale_order_line_report/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sales Order Line Report',
+    'version': '16.0.1.0.0',
+    'summary': 'Pivot report for sales order lines',
+    'category': 'Sales',
+    'author': 'Goja Solutions Limited',
+    'depends': ['sale_management'],
+    'data': [
+        'views/sale_order_line.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
