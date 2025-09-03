07f73b2864c912806c30dd520cdf6a698a5c7dc4
Gonzalo Siu <gonzalo.siu@gmail.com>
2025-09-03 19:10:54 +0800

    Trigger update 2

--- cw_sale_warning_date/__manifest__.py
+++ cw_sale_warning_date/__manifest__.py
@@ -6,7 +6,7 @@
     "license": "AGPL-3",
     "summary": "Custom sale warning date",
     "description": """Custom sale warning date.""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     'depends': ['sale', ],
     "data": [
     ],
