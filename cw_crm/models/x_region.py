6dcb725df6ab4ec27623c74a1d1764e5368aa6e0
Alvin Adji <alvinadjie@gmail.com>
2025-10-20 01:18:59 +0700

    [IMP] recreate studio table

---
+++ cw_crm/models/x_region.py
@@ -0,0 +1,10 @@
+from odoo import _, api, fields, models
+
+class Region(models.Model):
+    _name = 'x_region'
+    _description = 'Region'
+
+    x_name = fields.Char()
+    x_country_id = fields.Many2one('crm.lead', string='Country')
+    
+    
