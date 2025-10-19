5a1506181e6f7c059a61cc4e188e0d40e0190d27
Alvin Adji <alvinadjie@gmail.com>
2025-10-20 01:19:51 +0700

    [IMP] recreate studio table (2)

--- cw_crm/models/__init__.py
+++ cw_crm/models/__init__.py
@@ -4,3 +4,6 @@ from . import goja_region
 from . import product_template
 from . import product_pricelist_item
 from . import wine_type
+from . import x_wine_type
+from . import x_region
+from . import x_crm_lead_line
