4ca391292460bb85b74eeaf1bc2d06e5aa3c95dc
Alvin Adji <alvinadjie@gmail.com>
2025-10-22 01:01:20 +0700

    [IMP] flag on blank pricelist

--- goja_cw_crm/models/product_pricelist_item.py
+++ goja_cw_crm/models/product_pricelist_item.py
@@ -8,6 +8,8 @@ class ProductProcelistItem(models.Model):
     # x_type = fields.Selection(string='Selected Type', selection=[('Wholesale', 'Wholesale'), ('By bottle', 'By bottle'),('Pouring', 'Pouring')])
     
     
-    
-    
+class ProductProcelist(models.Model):
+    _inherit = 'product.pricelist'
+
+    goja_is_blank_pricelist = fields.Boolean('Is Blank Pricelist?')
     
