d705f6a0033663b0430c0ada6f6342b9b0952f8d
Alvin Adji <alvinadjie@gmail.com>
2025-12-18 22:38:30 +0700

    [MIG] migrate _get_options function

--- cw_customer_statement/models/res_partner.py
+++ cw_customer_statement/models/res_partner.py
@@ -34,7 +34,7 @@ class ResPartner(models.Model):
         self.ensure_one()
         data = []
         ar = self.env.ref('account_reports.aged_receivable_report')
-        options = ar._get_options()
+        options = ar.get_options()
         options['unfold_all'] = True
         options['partner'] = True
         options['partner_ids'] = [self.id]
