6f18fff73fceb17b43128fb1e860466c58fedd4a
Alvin Adji <alvinadjie@gmail.com>
2026-03-17 06:31:53 +0700

    [ADD] add module goja_cew_sale

---
+++ goja_cw_sale/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Continental Wines Sale',
+    'category': 'Custom',
+    "author": "Goja Solutions",
+    "version": "18.0.1.0.0",
+    'description': """
+    """,
+    'depends': [
+    	'sale_management',
+    ],
+    'data': [
+        'views/sale_views.xml',
+    ],
+    'license': 'OEEL-1',
+}
---
+++ goja_cw_sale/models/sale.py
@@ -0,0 +1,46 @@
+from odoo import models, fields, api
+import pytz
+from datetime import datetime, time
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    commitment_date_only = fields.Date(
+        string="Commitment Date",
+        compute="_compute_commitment_date_only",
+        search="_search_commitment_date_only",
+        store=False
+    )
+
+    @api.depends("commitment_date")
+    def _compute_commitment_date_only(self):
+        tz_name = self.env.user.tz or 'UTC'
+        user_tz = pytz.timezone(tz_name)
+        for rec in self:
+            if rec.commitment_date:
+                utc_dt = pytz.utc.localize(rec.commitment_date.replace(tzinfo=None))
+                rec.commitment_date_only = utc_dt.astimezone(user_tz).date()
+            else:
+                rec.commitment_date_only = False
+
+    def _search_commitment_date_only(self, operator, value):
+        if not value:
+            return []
+        
+        # Get the CURRENT user's timezone
+        tz_name = self.env.user.tz or 'UTC'
+        user_tz = pytz.timezone(tz_name)
+
+        # Convert picked date to UTC range
+        date_start = user_tz.localize(
+            datetime.combine(fields.Date.from_string(value), time.min)
+        ).astimezone(pytz.utc).replace(tzinfo=None)
+
+        date_end = user_tz.localize(
+            datetime.combine(fields.Date.from_string(value), time.max)
+        ).astimezone(pytz.utc).replace(tzinfo=None)
+
+        return [
+            ('commitment_date', '>=', date_start),
+            ('commitment_date', '<=', date_end),
+        ]
