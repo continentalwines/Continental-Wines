6163d241cc023ff6fb6eb4ea12e447c83d1c5b52
Gonzalo Siu <gonzalo.siu@gmail.com>
2026-03-26 19:49:04 +0800

    [IMP] crm pricing quotation report

--- goja_cw_crm/models/wine_type.py
+++ goja_cw_crm/models/wine_type.py
@@ -3,8 +3,10 @@ from odoo import _, api, fields, models
 class WineType(models.Model):
     _name = 'wine.type'
     _description = 'Wine Type'
+    _order = 'sequence, name'
 
     name = fields.Char()
+    sequence = fields.Integer(default=10)
     
     
     
