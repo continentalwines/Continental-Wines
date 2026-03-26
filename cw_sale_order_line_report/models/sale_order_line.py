0ed3a9961482eb12ec10984a0b20e0ec3f73f471
Alvin Adji <alvinadjie@gmail.com>
2026-03-26 18:50:22 +0700

    [IMP] delivery date add store true

--- cw_sale_order_line_report/models/sale_order_line.py
+++ cw_sale_order_line_report/models/sale_order_line.py
@@ -9,7 +9,8 @@ class SaleOrderLine(models.Model):
     delivery_address = fields.Char(related='order_id.partner_shipping_id.display_name', store=True, index=True, string="Delivery Address")
     commitment_date = fields.Datetime(
         string="Delivery Date",
-        related="order_id.commitment_date"
+        related="order_id.commitment_date",
+        store=True
     )
 
     commitment_date_only = fields.Date(
